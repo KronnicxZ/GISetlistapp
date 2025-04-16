0140b54697c29f9cc24d1ec94459564bf3811d8a
josem <kronnicxz@gmail.com>
2025-04-16 19:42:52 -0400

    fix: Corrige la funcionalidad de duplicación de canciones con nueva tonalidad

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -135,7 +135,7 @@ const SongDetails = ({ song, onClose, onDuplicateSong }) => {
               </div>
 
               {/* Duplicate Button */}
-              {isAdmin && semitones !== 0 && (
+              {isAdmin && semitones !== 0 && onDuplicateSong && (
                 <button
                   onClick={handleDuplicateWithNewKey}
                   className="w-full bg-[#1a1f2e] hover:bg-[#242937] text-[#FBAE00] p-4 rounded-lg flex items-center justify-center space-x-2"
